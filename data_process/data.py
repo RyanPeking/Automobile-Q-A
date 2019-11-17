2d09b1b39fcfd11e0371187f8d129c6aa58e6202
Mr. Wu <wushunyan@163.com>
2019-11-17 21:57:49 +0800

    batcher

---
+++ data_process/data.py
@@ -0,0 +1,108 @@
+class Vocab:
+    """Vocabulary class for mapping between words and ids (integers)"""
+    PAD_TOKEN = '[PAD]'
+    UNKNOWN_TOKEN = '[UNK]'
+    START_DECODING = '[START]'
+    STOP_DECODING = '[STOP]'
+
+    def __init__(self, vocab_file, max_size):
+        """Creates a vocab of up to max_size words, reading from the vocab_file. If max_size is 0, reads the entire vocab file.
+
+            Args:
+              vocab_file: path to the vocab file, which is assumed to contain "<word> <frequency>" on each line, sorted with most frequent word first. This code doesn't actually use the frequencies, though.
+              max_size: integer. The maximum size of the resulting Vocabulary."""
+
+        self.word2id = {Vocab.UNKNOWN_TOKEN: 0, Vocab.PAD_TOKEN: 1,
+                        Vocab.START_DECODING: 2, Vocab.STOP_DECODING: 3}
+        self.id2word = {0: Vocab.UNKNOWN_TOKEN, 1: Vocab.PAD_TOKEN, 2: Vocab.START_DECODING, 3: Vocab.STOP_DECODING}
+        self.count = 4
+
+        with open(vocab_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                pieces = line.split()
+                if len(pieces) != 2:
+                    print('Warning : incorrectly formatted line in vocabulary file : %s\n' % line)
+                    continue
+
+                w = pieces[0]
+                if w in [Vocab.UNKNOWN_TOKEN, Vocab.PAD_TOKEN, Vocab.START_DECODING, Vocab.STOP_DECODING]:
+                    raise Exception(
+                        '<s>, </s>, [UNK], [PAD], [START] and [STOP] shouldn\'t be in the vocab file, but %s is' % w)
+
+                if w in self.word2id:
+                    raise Exception('Duplicated word in vocabulary file: %s' % w)
+
+                self.word2id[w] = self.count
+                self.id2word[self.count] = w
+                self.count += 1
+                if max_size != 0 and self.count >= max_size:
+                    print("max_size of vocab was specified as %i; we now have %i words. Stopping reading." % (
+                    max_size, self.count))
+                    break
+
+        print("Finished constructing vocabulary of %i total words. Last word added: %s" % (
+        self.count, self.id2word[self.count - 1]))
+
+    def word_to_id(self, word):
+        if word not in self.word2id:
+            return self.word2id[Vocab.UNKNOWN_TOKEN]
+        return self.word2id[word]
+
+    def id_to_word(self, word_id):
+        if word_id not in self.id2word:
+            raise ValueError('Id not found in vocab: %d' % word_id)
+        return self.id2word[word_id]
+
+    def size(self):
+        return self.count
+
+
+
+def input_to_ids(article_words, vocab):
+    ids = []
+    oovs = []
+    unk_id = vocab.word_to_id(vocab.UNKNOWN_TOKEN)
+    for w in article_words:
+        i = vocab.word_to_id(w)
+        if i == unk_id:  # If w is OOV
+            if w not in oovs:  # Add to list of OOVs
+                oovs.append(w)
+            oov_num = oovs.index(w)  # This is 0 for the first article OOV, 1 for the second article OOV...
+            ids.append(
+                vocab.size() + oov_num)  # This is e.g. 50000 for the first article OOV, 50001 for the second...
+        else:
+            ids.append(i)
+    return ids, oovs
+
+def report_to_ids(abstract_words, vocab, article_oovs):
+    ids = []
+    unk_id = vocab.word_to_id(vocab.UNKNOWN_TOKEN)
+    for w in abstract_words:
+        i = vocab.word_to_id(w)
+        if i == unk_id:  # If w is an OOV word
+            if w in article_oovs:  # If w is an in-article OOV
+                vocab_idx = vocab.size() + article_oovs.index(w)  # Map to its temporary article OOV number
+                ids.append(vocab_idx)
+            else:  # If w is an out-of-article OOV
+                ids.append(unk_id)  # Map to the UNK token id
+        else:
+            ids.append(i)
+    return ids
+
+def output_to_words(id_list, vocab, article_oovs):
+    words = []
+    for i in id_list:
+        try:
+            w = vocab.id_to_word(i)  # might be [UNK]
+        except ValueError as e:  # w is OOV
+            assert article_oovs is not None, "Error: model produced a word ID that isn't in the vocabulary. This should not happen in baseline (no pointer-generator) mode"
+            article_oov_idx = i - vocab.size()
+            try:
+                w = article_oovs[article_oov_idx]
+            except ValueError as e:  # i doesn't correspond to an article oov
+                raise ValueError(
+                    'Error: model produced word ID %i which corresponds to article OOV %i but this example only has %i article OOVs' % (
+                    i, article_oov_idx, len(article_oovs)))
+        words.append(w)
+    return words
+
---
+++ seq2seq_tf2/batcher.py
@@ -0,0 +1,181 @@
+import tensorflow as tf
+from data_process.data import input_to_ids, report_to_ids, Vocab
+import pandas as pd
+
+
+def pad_decoder_inp_targ(dec_input, target, max_len, pad_id):
+    """Pad decoder input and target sequences with pad_id up to max_len."""
+    while len(dec_input) < max_len:
+        dec_input.append(pad_id)
+    while len(target) < max_len:
+        target.append(pad_id)
+    return dec_input, target
+
+
+def pad_encoder_input(enc_input, enc_input_extend_vocab, max_len, pad_id):
+    """Pad the encoder input sequence with pad_id up to max_len."""
+    while len(enc_input) < max_len:
+        enc_input.append(pad_id)
+    while len(enc_input_extend_vocab) < max_len:
+        enc_input_extend_vocab.append(pad_id)
+    return enc_input, enc_input_extend_vocab
+
+
+def get_dec_inp_targ_seqs(sequence, max_len, start_id, stop_id):
+    """
+    Given the reference summary as a sequence of tokens, return the input sequence for the decoder, and the target sequence which we will use to calculate loss. The sequence will be truncated if it is longer than max_len. The input sequence must start with the start_id and the target sequence must end with the stop_id (but not if it's been truncated).
+    Args:
+      sequence: List of ids (integers)
+      max_len: integer
+      start_id: integer
+      stop_id: integer
+    Returns:
+      inp: sequence length <=max_len starting with start_id
+      target: sequence same length as input, ending with stop_id only if there was no truncation
+    """
+    inp = [start_id] + sequence[:]
+    target = sequence[:]
+    if len(inp) > max_len:  # truncate
+        inp = inp[:max_len]
+        target = target[:max_len]  # no end_token
+    else:  # no truncation
+        target.append(stop_id)  # end token
+    assert len(inp) == len(target)
+    return inp, target
+
+
+def example_generator(filename, vocab, max_enc_len, max_dec_len, batch_size, mode):
+    data = pd.read_csv(filename)
+
+    if mode == "train":
+        train_dataset = tf.data.Dataset.zip((data['input'], data['Report']))
+    elif mode == "test":
+        train_dataset = tf.data.Dataset(data['input'])
+    train_dataset = train_dataset.shuffle(batch_size, reshuffle_each_iteration=True).repeat()
+
+    for raw_record in train_dataset:
+        input = raw_record[0]
+        report = raw_record[1]
+
+        start_decoding = vocab.word_to_id(vocab.START_DECODING)
+        stop_decoding = vocab.word_to_id(vocab.STOP_DECODING)
+        pad_decoding = vocab.word_to_id(vocab.PAD_TOKEN)
+
+        input_words = input.split()[:max_enc_len]
+        enc_len = len(input_words)
+        enc_input = [vocab.word_to_id(w) for w in input_words]
+        enc_input_extend_vocab, input_oovs = input_to_ids(input_words, vocab)
+        enc_input, enc_input_extend_vocab = pad_decoder_inp_targ(enc_input, enc_input_extend_vocab, max_enc_len, pad_decoding)
+
+        report_words = report.split()
+        report_ids = [vocab.word_to_id(w) for w in report_words]
+        report_ids_extend_vocab = report_to_ids(report_words, vocab, input_oovs)
+        dec_input, target = get_dec_inp_targ_seqs(report_ids, max_dec_len, start_decoding, stop_decoding)
+        _, target = get_dec_inp_targ_seqs(report_ids_extend_vocab, max_dec_len, start_decoding, stop_decoding)
+        dec_len = len(dec_input)
+        dec_input, target = pad_decoder_inp_targ(dec_input, target, max_dec_len, pad_decoding)
+
+        output = {
+            "enc_len": enc_len,
+            "enc_input": enc_input,
+            "enc_input_extend_vocab": enc_input_extend_vocab,
+            "input_oovs": input_oovs,
+            "dec_input": dec_input,
+            "target": target,
+            "dec_len": dec_len,
+            "input": output,
+            "report": report,
+        }
+        if mode == "test":
+            for _ in range(batch_size):
+                yield output
+        else:
+            yield output
+
+
+def batch_generator(generator, filename, vocab, max_enc_len, max_dec_len, batch_size, mode):
+    dataset = tf.data.Dataset.from_generator(lambda: generator(filename, vocab, max_enc_len, max_dec_len, batch_size, mode),
+                                             output_types={
+                                                 "enc_len": tf.int32,
+                                                 "enc_input": tf.int32,
+                                                 "enc_input_extend_vocab": tf.int32,
+                                                 "input_oovs": tf.string,
+                                                 "dec_input": tf.int32,
+                                                 "target": tf.int32,
+                                                 "dec_len": tf.int32,
+                                                 "input": tf.string,
+                                                 "report": tf.string,
+                                             },
+                                             output_shapes={
+                                                 "enc_len": [],
+                                                 "enc_input": [None],
+                                                 "enc_input_extend_vocab": [None],
+                                                 "input_oovs": [None],
+                                                 "dec_input": [None],
+                                                 "target": [None],
+                                                 "dec_len": [],
+                                                 "input": [],
+                                                 "report": [],
+                                             })
+
+    dataset = dataset.padded_batch(batch_size,
+                                   padded_shapes=({"enc_len": [],
+                                                   "enc_input": [None],
+                                                   "enc_input_extend_vocab": [None],
+                                                   "input_oovs": [None],
+                                                   "dec_input": [max_dec_len],
+                                                   "target": [max_dec_len],
+                                                   "dec_len": [],
+                                                   "input": [],
+                                                   "report": []}),
+                                   padding_values={"enc_len": -1,
+                                                   "enc_input": 1,
+                                                   "enc_input_extend_vocab": 1,
+                                                   "input_oovs": b'',
+                                                   "dec_input": 1,
+                                                   "target": 1,
+                                                   "dec_len": -1,
+                                                   "input": b"",
+                                                   "report": b""},
+                                   drop_remainder=True)
+
+    def update(entry):
+        return ({"enc_input": entry["enc_input"],
+                 "extended_enc_input": entry["enc_input_extend_vocab"],
+                 "input_oovs": entry["input_oovs"],
+                 "enc_len": entry["enc_len"],
+                 "input": entry["input"],
+                 "max_oov_len": tf.shape(entry["input_oovs"])[1]},
+
+                {"dec_input": entry["dec_input"],
+                 "dec_target": entry["target"],
+                 "dec_len": entry["dec_len"],
+                 "report": entry["report"]})
+
+    dataset = dataset.map(update)
+    return dataset
+
+
+def batcher(filename, vocab_path, hps):
+    # hps: hyperparameters
+    vocab = Vocab(vocab_path, hps['max_size'])
+    dataset = batch_generator(example_generator, filename, vocab, hps["max_enc_len"],
+                              hps["max_dec_len"], hps["batch_size"], hps["mode"])
+    return dataset
+
+
+if __name__ == '__main__':
+    import os
+    import argparse
+    train_path = os.path.join(os.path.abspath('../'), 'data', 'train.csv')
+    vocab_path = os.path.join(os.path.abspath('../'), 'data', 'words_frequences.txt')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--max_enc_len", default=500, help="Encoder input max sequence length", type=int)
+    parser.add_argument("--max_dec_len", default=50, help="Decoder input max sequence length", type=int)
+    parser.add_argument("--max_size", default=50000, help="Vocabulary size", type=int)
+    parser.add_argument("--batch_size", default=32, help="batch size", type=int)
+    parser.add_argument("--mode", default='train', help="batch size")
+    args = parser.parse_args()
+    hps = vars(args)
+    # print(hps['max_size'])
+    b = batcher(train_path, vocab_path, hps)
