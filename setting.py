0daca3eea5479c778a986bd1685495e030efacb9
Mr. Wu <wushunyan@163.com>
2019-11-21 20:29:18 +0800

    del useless

--- setting.py
+++
@@ -1,8 +0,0 @@
-import os
-
-max_features = 300
-max_len = 300
-embed_size = 100
-max_length_inp,max_length_targ= 500, 50
-train_path = os.path.join(os.path.abspath('./'), 'data', 'train.csv')
-model_path = os.path.join(os.path.abspath('./'), 'data', 'w2v.model')
